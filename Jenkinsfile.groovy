f47525e91bbae51f4cf20f637658fad0880e3b93
abanobmorkosgad <abanobmorkos13@gmail.com>
2024-03-12 20:50:33 +0200

    dfgf

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,4 +1,3 @@
-def gv
 pipeline{
     agent any
     tools{
@@ -17,7 +16,7 @@ pipeline{
             steps {
                 script{
                     echo "building docker image..."
-                    withCredentials([usernamePassword(credentailsId: "Docker-cred", passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+                    withCredentials([usernamePassword(credentialsId: "Docker-cred", passwordVariable: 'PASS', usernameVariable: 'USER')]) {
                         sh "docker build -t abanobmorkos10/myrepo:2.0 ."
                         sh "docker login -u $USER --password $PASS"
                         sh "docker push abanobmorkos10/myrepo:2.0"
