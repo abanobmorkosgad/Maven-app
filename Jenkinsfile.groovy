d519140622c180aaad478d6f2be63ef94afa84f0
abanobmorkosgad <abanobmorkos13@gmail.com>
2024-03-12 22:20:12 +0200

    dfgf

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -17,11 +17,9 @@ pipeline{
                 script{
                     echo "building docker image..."
                     withCredentials([usernamePassword(credentialsId: "Docker-cred", passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-                        withEnv(["DOCKER_PASSWORD=${PASS}"]) {
-                            sh "docker build -t abanobmorkos10/myrepo:2.0 ."
-                            sh "docker login -u $USER --password-stdin <<< \$DOCKER_PASSWORD"
-                            sh "docker push abanobmorkos10/myrepo:2.0"
-                        }
+                        sh "docker build -t abanobmorkos10/myrepo:2.0 ."
+                        sh "echo $PASS | docker login -u $USER --password-stdin"
+                        sh "docker push abanobmorkos10/myrepo:2.0"
                     }
                 }
             }
